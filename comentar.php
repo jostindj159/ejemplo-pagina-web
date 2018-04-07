274a62b7deb8149d97856ae637c42cbe0cc27414
jostindj159 <36160682+jostindj159@users.noreply.github.com>
2018-04-06 19:47:33 -0500

    Add files via upload

---
+++ comentar.php
@@ -0,0 +1,18 @@
+<?php
+$comentario=strip_tags($_POST['comentario']);
+$usuario=strip_tags($_POST['usuario']);
+
+if($comentario=="" or $usuario==""){	
+echo "<script type='text/javascript'>alert('Llene los campos');window.location.href='index.php'</script>";
+}else{
+
+$grabar = fopen("comentarios.html","a");
+	
+fwrite($grabar,"<p><strong>$usuario</strong><br>$comentario</p>");
+
+fclose($grabar);
+
+include("index.php");
+	
+}
+?>
---
+++ index.php
@@ -0,0 +1,142 @@
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href="css/stilos.css">
+<div class="container">
+		<center><h1>logo</h1></center>
+</div>
+</head>	
+	
+<body>
+	
+	<header>
+<!-- -->
+<div class="container">
+		<div id="header">
+			<nav> <!-- Aqui estamos iniciando la nueva etiqueta nav -->
+				<ul class="nav">
+					<li><a href="">Inicio</a></li>
+					<li><a href="">Softwares</a>
+						<ul>
+							<li><a href="">Submenu1</a></li>
+							<li><a href="">Submenu2</a></li>
+							<li><a href="">Submenu3</a></li>
+							<li><a href="">Submenu4</a>
+								<ul>
+									<li><a href="">Submenu1</a></li>
+									<li><a href="">Submenu2</a></li>
+									<li><a href="">Submenu3</a></li>
+									<li><a href="">Submenu4</a></li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+					<li><a href="">Clases</a>
+						<ul>
+							<li><a href="">Submenu1</a></li>
+							<li><a href="">Submenu2</a></li>
+							<li><a href="">Submenu3</a></li>
+							<li><a href="">Submenu4</a></li>
+						</ul>
+					</li>
+					<li><a href="">Contacto</a></li>
+					<li><a href="">Admin</a></li>
+				</ul>
+			</nav><!-- Aqui estamos cerrando la nueva etiqueta nav -->
+		</div>
+</div>
+<!-- -->
+	</header>
+	
+<div class="container"><br>
+	<section class="main row">
+		<!--columas //xs= extra small//sm=small//md=mediano//lglargo-->
+		<article class="col-xs-12 col-sm-4 col-md-4">
+			<h3>COLUMNA</h3>
+			<p>
+				hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola 
+				hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola 
+				hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola 
+				hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola 
+				hola holad hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola 
+				hola holad hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola 
+				hola holad hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola  
+				hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola 
+				hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola 
+				hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola 
+			</p>
+		</article>
+		<article class="col-xs-12 col-sm-4 col-md-4">
+			<h3>COLUMNA</h3>
+			<p>
+				hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola 
+				hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola 
+				hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola 
+				hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola 
+				hola holad hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola 
+				hola holad hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola 
+				hola holad hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola  
+				hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola 
+				hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola 
+				hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola 
+			</p>
+		</article>
+		<article class=" color1 col-xs-12 col-sm-4 col-md-4">
+			<h3>COLUMNA</h3>
+			<p>
+				hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola 
+				hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola 
+				hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola 
+				hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola 
+				hola holad hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola 
+				hola holad hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola 
+				hola holad hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola  
+				hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola 
+				hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola 
+				hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola hola 
+			</p>
+		</article>
+	</section>
+	
+</div><hr>
+<!-- Comentario -->
+<!-- Comentario -->
+<!-- Comentario -->
+<div class="container">
+		<article class="col-xs-12 col-sm-4 col-md-4">
+			<h3>Comentarios</h3>
+	<form method="post" action="comentar.php">
+	<label>Usuario:</label><br>
+	<input type="text" name="usuario" placeholder="usuario"><p/>
+	<label>Comentario:</label><br>
+	<textarea name="comentario" placeholder="Escribe tu comentario" cols="40" rows="3"></textarea><p/>
+
+	<input type="submit" value="publicar">
+	</form>
+		</article>
+		
+		<article class="col-xs-12 col-sm-8 col-md-8">
+			<p>
+			<?php include("comentarios.html");?>	
+			</p>
+		</article>
+	
+</div>
+	
+	<footer>
+		<div class="container">
+			<h3>JOSTHEIN MAYORCA BELLEZA</h3>
+		</div>
+	</footer>
+</body>
+	
+	
+	<!-- Compilar JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+	
+</html>
